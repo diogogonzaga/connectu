ffbaabdf8e59f4a84e43c606a4f1e3f9a4b5a9e7
Diogo Gonzaga <diogodgonzaga@gmail.com>
2025-12-19 23:08:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const navList = document.querySelector('.nav-list');
-const navLinks = document.querySelectorAll('.nav-list li');
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
 
-mobileMenu.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
-  navList.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+});
 
-  navLinks.forEach((link, index) => {
-    link.style.animation = link.style.animation ? '' : `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+// Fechar menu ao clicar num link
+document.querySelectorAll('#navMenu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    menuToggle.classList.remove('active');
   });
 });
